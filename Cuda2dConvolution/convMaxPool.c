ad7c2ad937500ed3fe30a5174e56d1a3b0befd4f
Miguel <miguel.nunez@student.pxl.be>
2022-12-27 15:51:58 +0100

    test comment

--- Cuda2dConvolution/convMaxPool.c
+++ Cuda2dConvolution/convMaxPool.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include "stb_image.h"
 #include "stb_image_write.h"
+#include <math.h>
+
+
+void convMax()
+{
+    // tthis is coment
+}
 
 int main(int argc, char *argv[])
 {
-    
+
 }
