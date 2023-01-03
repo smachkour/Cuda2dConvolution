f32d8b2d7dba20a4f0df66ee8d35f138a4c27897
Miguel <miguel.nunez@student.pxl.be>
2023-01-03 14:50:11 +0100

    c maxpool

--- Cuda2dConvolution/convMaxPoolv2.c
+++ Cuda2dConvolution/convMaxPoolv2.c
@@ -1,60 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <png.h">
+#include "stb_image.h"
+#include "stb_image_write.h"
 
-#define WIDTH 800
-#define HEIGHT 600
-#define DEPTH 3
-#define POOL_SIZE 2
+int main(int argc, char** argv) {
 
-int main() {
-  // Load the image
-  png_image image;
-  image.version = PNG_IMAGE_VERSION;
-  image.format = PNG_FORMAT_RGB;
-  image.width = WIDTH;
-  image.height = HEIGHT;
-  png_image_begin_read_from_file(&image, "Before.png");
-  png_bytep buffer = malloc(PNG_IMAGE_SIZE(image));
-  png_image_finish_read(&image, NULL, buffer, 0, NULL);
+  // Load the input image using stb_image.h
+  int width, height, num_channels;
+  unsigned char* image = stbi_load("Before.png", &width, &height, &num_channels, 4);
+  if (image == NULL) {
+    fprintf(stderr, "Failed to load image %s\n", argv[1]);
+    return 1;
+  }
 
-  // Create the output image
-  png_image output_image;
-  output_image.version = PNG_IMAGE_VERSION;
-  output_image.format = PNG_FORMAT_RGB;
-  output_image.width = WIDTH / POOL_SIZE;
-  output_image.height = HEIGHT / POOL_SIZE;
-  png_bytep output_buffer = malloc(PNG_IMAGE_SIZE(output_image));
+  // Create an output image with half the width and height of the input
+  int output_width = width / 2;
+  int output_height = height / 2;
+  unsigned char* output_image = malloc(output_width * output_height * num_channels);
 
-  // Perform the maxpooling
-  for (int y = 0; y < HEIGHT; y += POOL_SIZE) {
-    for (int x = 0; x < WIDTH; x += POOL_SIZE) {
-      for (int d = 0; d < DEPTH; ++d) {
-        // Find the maximum value in the pooling window
+  // Perform max pooling on the input image and store the result in the output image
+  for (int y = 0; y < output_height; y++) {
+    for (int x = 0; x < output_width; x++) {
+      for (int c = 0; c < num_channels; c++) {
         int max_value = 0;
-        for (int dy = 0; dy < POOL_SIZE; ++dy) {
-          for (int dx = 0; dx < POOL_SIZE; ++dx) {
-            int value = buffer[(y + dy) * WIDTH * DEPTH + (x + dx) * DEPTH + d];
+        // Take the maximum value of the 2x2 group of pixels centered at (2x, 2y)
+        for (int dy = 0; dy < 2; dy++) {
+          for (int dx = 0; dx < 2; dx++) {
+            int value = image[(2 * y + dy) * width + (2 * x + dx) * num_channels + c];
             if (value > max_value) {
               max_value = value;
             }
           }
         }
-
-        // Store the maximum value in the output image
-        output_buffer[(y / POOL_SIZE) * output_image.width * DEPTH + (x / POOL_SIZE) * DEPTH + d] = max_value;
+        output_image[y * output_width + x * num_channels + c] = max_value;
       }
     }
   }
 
-  // Save the output image
-  png_image_write_to_file(&output_image, "output.png", 0, output_buffer, 0, NULL);
+  // Save the output image using stb_image.h
+      // Write image back to disk
+    printf("Writing png to disk...\r\n");
+
+    stbi_write_png("output_image.png", output_width, output_height, num_channels, output_image, output_width);
+  
+  if (!stbi_write_png("output_image.png", output_width, output_height, num_channels, output_image, 0)) {
+    fprintf(stderr, "Failed to save image %s\n", output_image);
+    return 1;
+  }
 
   // Clean up
-  free(buffer);
-  free(output_buffer);
-  png_image_free(&image);
-  png_image_free(&output_image);
+  stbi_image_free(image);
+  free(output_image);
 
   return 0;
 }
